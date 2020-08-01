fc58c606fde858fbea8a04518e194a4a06acc27c
asultan00 <asultan00@stuy.edu>
2020-08-01 19:10:48 -0400

    added some more app things

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require('express');
+const nunjucks = require('nunjucks');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+// configure nunjucks
+nunjucks.configure("views", {
+     autoescape: true,
+     express: app,
+});
+
+// routes
+app.use("/", require("./routes/index"));
+
+app.use("/css", express.static(__dirname + "/css"));
+
+// 404 error handling
+app.use(function (req, res, next) {
+     res.status(404).send("Sorry can't find that!");
+});
+
+app.get;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
