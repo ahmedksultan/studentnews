1b43393aff90233be5db776ca64e82e1fd23519a
asultan00 <asultan00@stuy.edu>
2020-08-01 19:15:31 -0400

    changed listen and removed extraneous get

--- app.js
+++ app.js
@@ -20,7 +20,5 @@ app.use(function (req, res, next) {
      res.status(404).send("Sorry can't find that!");
 });
 
-app.get;
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+app.listen(PORT, console.log(`Example app listening at http://localhost:${PORT}`));
